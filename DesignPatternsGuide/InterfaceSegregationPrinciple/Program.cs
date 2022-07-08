db4f5c8c7a2c1b5cc12837c432e394b45e6334d0
Alberto Espinoza <aespinoza96@outlook.com>
2022-07-07 23:58:39 -0600

    InterfaceSegregationPrinciple

---
+++ DesignPatternsGuide/InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * The idea to demonstrate the Interface Segregation Principle is to create several interfaces which allow us
+ * to define classes with only the necessary functionality. For example, there could be an interface "IMachine"
+ * that do print, fax and scan. But then, a type of machine can't scan so the implementation of this method won't
+ * be added. To fix this, separate interfaces are created and then every machine class can implement what they actually
+ * need as it is explain in the code.
+ */
+
+namespace InterfaceSegregationPrinciple
+{
+  public class Document
+  {
+  }
+
+  public interface IMachine
+  {
+    void Print(Document d);
+    void Fax(Document d);
+    void Scan(Document d);
+  }
+
+  // ok if you need a multifunction machine
+  public class MultiFunctionPrinter : IMachine
+  {
+    public void Print(Document d)
+    {
+      //
+    }
+
+    public void Fax(Document d)
+    {
+      //
+    }
+
+    public void Scan(Document d)
+    {
+      //
+    }
+  }
+
+  // Type of machine that cannot scan.
+  public class OldFashionedPrinter : IMachine
+  {
+    public void Print(Document d)
+    {
+      // yep
+    }
+
+    public void Fax(Document d)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Scan(Document d)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+
+  // Interfaces with specific definitions.
+  public interface IPrinter
+  {
+    void Print(Document d);
+  }
+
+  public interface IScanner
+  {
+    void Scan(Document d);
+  }
+
+  public class Printer : IPrinter
+  {
+    public void Print(Document d)
+    {
+      
+    }
+  }
+
+  public class Photocopier : IPrinter, IScanner
+  {
+    public void Print(Document d)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public void Scan(Document d)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+
+  public interface IMultiFunctionDevice : IPrinter, IScanner //
+  {
+    
+  }
+
+  public struct MultiFunctionMachine : IMultiFunctionDevice
+  {
+    // compose this out of several modules
+    private IPrinter printer;
+    private IScanner scanner;
+
+    public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+    {
+      if (printer == null)
+      {
+        throw new ArgumentNullException(paramName: nameof(printer));
+      }
+      if (scanner == null)
+      {
+        throw new ArgumentNullException(paramName: nameof(scanner));
+      }
+      this.printer = printer;
+      this.scanner = scanner;
+    }
+
+    public void Print(Document d)
+    {
+      printer.Print(d);
+    }
+
+    public void Scan(Document d)
+    {
+      scanner.Scan(d);
+    }
+  }
+}
