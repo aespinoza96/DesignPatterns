c5e88df91ae0caf8aa9bafe465d25be36aa5dcc8
Alberto Espinoza <aespinoza96@outlook.com>
2022-08-06 22:18:54 -0600

    Template Method Pattern

---
+++ DesignPatternsGuide/TemplateMethod/Program.cs
@@ -0,0 +1,100 @@
+﻿/*
+    Template Method Coding Exercise
+    Imagine a typical collectible card game which has cards representing creatures. Each creature has two values: 
+    Attack and Health. Creatures can fight each other, dealing their Attack damage, thereby reducing their opponent's 
+    health.
+    
+    The class CardGame implements the logic for two creatures fighting one another. However, the exact mechanics of 
+    how damage is dealt is different:
+    
+    TemporaryCardDamage : In some games (e.g., Magic: the Gathering), unless the creature has been killed, its health 
+    returns to the original value at the end of combat.
+    PermanentCardDamage : In other games (e.g., Hearthstone), health damage persists.
+    You are asked to implement classes TemporaryCardDamageGame  and PermanentCardDamageGame  that would allow us to 
+    simulate combat between creatures.
+    
+    Some examples:
+    
+    With temporary damage, creatures 1/2 and 1/3 can never kill one another. With permanent damage, second creature 
+    will win after 2 rounds of combat.
+    With either temporary or permanent damage, two 2/2 creatures kill one another.
+*/
+
+namespace TemplateMethod;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
+public class Creature
+{
+    public int Attack, Health;
+
+    public Creature(int attack, int health)
+    {
+        Attack = attack;
+        Health = health;
+    }
+}
+
+public abstract class CardGame
+{
+    public Creature[] Creatures;
+
+    public CardGame(Creature[] creatures)
+    {
+        Creatures = creatures;
+    }
+
+    // returns -1 if no clear winner (both alive or both dead)
+    public int Combat(int creature1, int creature2)
+    {
+        Creature first = Creatures[creature1];
+        Creature second = Creatures[creature2];
+        Hit(first, second);
+        Hit(second, first);
+        bool firstAlive = first.Health > 0;
+        bool secondAlive = second.Health > 0;
+        if (firstAlive == secondAlive) return -1;
+        return firstAlive ? creature1 : creature2;
+    }
+
+    // attacker hits other creature
+    protected abstract void Hit(Creature attacker, Creature other);
+}
+
+public class TemporaryCardDamageGame : CardGame
+{
+    public TemporaryCardDamageGame(Creature[] creatures) : base(creatures)
+    {
+        Creatures = creatures;
+    }
+
+    protected override void Hit(Creature attacker, Creature other)
+    {
+        var initialHealth = other.Health;
+        other.Health -= attacker.Attack;
+        if (other.Health > 0)
+        {
+            other.Health = initialHealth;
+        }
+    }
+}
+
+public class PermanentCardDamage : CardGame
+{
+    public PermanentCardDamage(Creature[] creatures) : base(creatures)
+    {
+        Creatures = creatures;
+    }
+
+    protected override void Hit(Creature attacker, Creature other)
+    {
+        other.Health -= attacker.Attack;
+    }
+}
+
