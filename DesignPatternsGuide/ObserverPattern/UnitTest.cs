3e7fb79ace423f318538d1c02289a67964ad098a
Alberto Espinoza <aespinoza96@outlook.com>
2022-08-06 18:29:33 -0600

    Observer Pattern

---
+++ DesignPatternsGuide/ObserverPattern/UnitTest.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+
+namespace ObserverPattern;
+
+[TestFixture]
+public class UnitTest
+{
+    [Test]
+    public void TestPattern()
+    {
+        Assert.That(typeof(Game).GetFields(), Is.Empty);
+        Assert.That(typeof(Game).GetProperties(), Is.Empty);
+    }
+    
+    [Test]
+    public void SingleRatTest()
+    {
+        var game = new Game();
+        var rat = new Rat(game);
+        Assert.That(rat.Attack, Is.EqualTo(1));
+    }
+      
+    [Test]
+    public void TwoRatTest()
+    {
+        var game = new Game();
+        var rat = new Rat(game);
+        var rat2 = new Rat(game);
+        Assert.That(rat.Attack, Is.EqualTo(2));
+        Assert.That(rat2.Attack, Is.EqualTo(2));
+    }
+  
+    [Test]
+    public void ThreeRatsOneDies()
+    {
+        var game = new Game();
+        
+        var rat = new Rat(game);
+        Assert.That(rat.Attack, Is.EqualTo(1));
+        
+        var rat2 = new Rat(game);
+        Assert.That(rat.Attack, Is.EqualTo(2));
+        Assert.That(rat2.Attack, Is.EqualTo(2));
+  
+        using (var rat3 = new Rat(game))
+        {
+            Assert.That(rat.Attack, Is.EqualTo(3));
+            Assert.That(rat2.Attack, Is.EqualTo(3));
+            Assert.That(rat3.Attack, Is.EqualTo(3));
+        }
+        
+        Assert.That(rat.Attack, Is.EqualTo(2));
+        Assert.That(rat2.Attack, Is.EqualTo(2));
+    }
+}
